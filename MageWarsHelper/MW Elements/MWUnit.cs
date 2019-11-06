834aa67dfbed74ead5e0297d4a49251681b24561
EternityDoom <EternityDoom@users.noreply.github.com>
2019-11-06 15:17:49 -0700

    Updated MWUnit (and by extension, MWCreature and MWMage) to have life, damage, mana, channeling, armor, incorporeality, and alive properties.

--- MageWarsHelper/MW Elements/MWUnit.cs
+++ MageWarsHelper/MW Elements/MWUnit.cs
@@ -8,6 +8,61 @@ namespace MageWarsHelper
 {
     public class MWUnit : MWCard
     {
-
+        public bool Incorporeal { get; set; }
+        private int life = 1, damage = 0, mana = 0, channeling = 0, armor = 0;
+        public int Life
+        {
+            get { return life; }
+            set
+            {
+                if (value < 1) life = 1;
+                else life = value;
+            }
+        }
+        public int Damage
+        {
+            get { return damage; }
+            set
+            {
+                if (value < 0) damage = 0;
+                else damage = value;
+            }
+        }
+        public int Mana
+        {
+            get { return mana; }
+            set
+            {
+                if (value < 0) mana = 0;
+                else mana = value;
+            }
+        }
+        public int Channeling
+        {
+            get { return channeling; }
+            set
+            {
+                if (value < 0) channeling = 0;
+                else channeling = value;
+            }
+        }
+        public int Armor
+        {
+            get { return armor; }
+            set
+            {
+                if (Incorporeal || value < 0) armor = 0;
+                else armor = value;
+            }
+        }
+        public bool Alive
+        {
+            get { return life > damage; }
+            set
+            {
+                if (!value && life > damage) damage = life;
+                else if (value && damage >= life) damage = 0;
+            }
+        }
     }
 }
