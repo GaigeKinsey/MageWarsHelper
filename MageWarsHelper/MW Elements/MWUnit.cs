ed19ea703a185022579abf09faca2bc898a01331
EternityDoom <EternityDoom@users.noreply.github.com>
2019-11-13 14:50:36 -0700

    Fixed life and armor handling for MWUnit, and added element modifiers and immunities. Also moved mana and associated properties to MWCard, since some equipment has mana in expansion sets.

--- MageWarsHelper/MW Elements/MWUnit.cs
+++ MageWarsHelper/MW Elements/MWUnit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +9,36 @@ namespace MageWarsHelper
 {
     public class MWUnit : MWCard
     {
-        /// <summary>
-        /// If true, can't have armor and 2s rolled on damage dice only count if the attack is ethereal.
-        /// </summary>
+        private Dictionary<AttackElement, int> elementModifiers = new Dictionary<AttackElement, int>();
+        private bool nonliving = false;
+        private int life = 1, damage = 0, regen = 0, armor = 0;
         /// <summary>
         /// If true, only one copy can be in play at a time.
         /// </summary>
         public bool Legendary { get; set; }
         /// <summary>
-        /// If true, can't be healed and can't regenerate, but can be repaired.
+        /// If true, can't be healed and can't regenerate, but can be repaired and have poison immunity.
         /// </summary>
-        public bool Nonliving { get; set; }
-        private int life = 1, damage = 0, mana = 0, channeling = 0, armor = 0;
+        public bool Nonliving {
+            get
+            {
+                return nonliving;
+            }
+            set
+            {
+                if (value)
+                {
+                    if (regen > 0)
+                    {
+                        regen = 0;
+                        FieldChanged("Regenerate");
+                    }
+                    nonliving = true;
+                }
+                else nonliving = false;
+                FieldChanged();
+            }
+        }
         /// <summary>
         /// The maximum damage that this unit can take.
         /// </summary>
@@ -30,6 +49,7 @@ namespace MageWarsHelper
             {
                 if (value < 1) life = 1;
                 else life = value;
+                FieldChanged("Alive");
                 FieldChanged();
             }
         }
@@ -43,47 +63,31 @@ namespace MageWarsHelper
             {
                 if (value < 0) damage = 0;
                 else damage = value;
+                FieldChanged("Alive");
                 FieldChanged();
             }
         }
-        public bool HasMana
+        /// <summary>
+        /// How much damage this unit heals during upkeep.
+        /// </summary>
+        public int Regenerate
         {
             get
             {
-                return mana + channeling > 0;
+                return regen;
             }
             set
             {
-                if (!value)
+                if (Nonliving || value < 0)
                 {
-                    channeling = 0;
-                    mana = 0;
-                } else if (value && channeling == 0)
-                {
-                    channeling = 1;
+                    regen = 0;
                 }
+                else regen = value;
             }
         }
-        public int Mana
-        {
-            get { return mana; }
-            set
-            {
-                if (value < 0) mana = 0;
-                else mana = value;
-                FieldChanged();
-            }
-        }
-        public int Channeling
-        {
-            get { return channeling; }
-            set
-            {
-                if (value < 0) channeling = 0;
-                else channeling = value;
-                FieldChanged();
-            }
-        }
+        /// <summary>
+        /// If false, armor will always be treated as 0.
+        /// </summary>
         public bool HasArmor
         {
             get
@@ -96,14 +100,20 @@ namespace MageWarsHelper
                 {
                     armor = 0;
                     Incorporeal = false;
+                    FieldChanged("Incorporeal");
                 } else if (!value && armor >= 0)
                 {
                     armor = -1;
                 }
+                FieldChanged("Armor");
+                FieldChanged();
             }
         }
+        /// <summary>
+        /// If true, can't have armor and 2s rolled on damage dice only count if the attack is ethereal.
+        /// </summary>
         public bool Incorporeal {
-            get 
+            get
             {
                 return armor > -1;
             }
@@ -119,30 +129,115 @@ namespace MageWarsHelper
                         armor = -1;
                     }
                 }
+                FieldChanged("HasArmor");
+                FieldChanged("Armor");
+                FieldChanged();
             }
         }
+        /// <summary>
+        /// The amount subtracted from incoming damage. Critical or direct damage ignores this.
+        /// </summary>
         public int Armor
         {
-            get 
+            get
             {
                 if (Incorporeal) return 0;
-                return armor; 
+                return armor;
             }
             set
             {
-                if (Incorporeal || value < 0) armor = 0;
+                if (!HasArmor || value < 0) armor = 0;
                 else armor = value;
                 FieldChanged();
             }
         }
+        /// <summary>
+        /// Whether this unit has any life remaining after damage. If it's switched
+        /// from false to true, the unit revives with 0 damage.
+        /// </summary>
         public bool Alive
         {
             get { return life > damage; }
             set
             {
-                if (!value && life > damage) damage = life;
+                if (!value && life > damage) { 
+                    damage = life;
+                }
                 else if (value && damage >= life) damage = 0;
+                FieldChanged("Damage");
                 FieldChanged();
+            }
+        }
+        /// <summary>
+        /// Figures out how many dice to add or remove from attacks of a given element that
+        /// are made against this unit. If this unit is immune to that element, this returns
+        /// null.
+        /// </summary>
+        /// <param name="element">The element to check</param>
+        /// <returns>Null for immune, otherwise the number of dice to add or remove</returns>
+        public int? GetElementModifier(AttackElement element)
+        {
+            if (elementModifiers.ContainsKey(element))
+            {
+                return elementModifiers[element];
+            }
+            else return null;
+        }
+        /// <summary>
+        /// Increases or decreases the number of dice rolled by attacks of a given element against.
+        /// this unit. Positive numbers increase, negative numbers decrease. Null makes this unit
+        /// immune to that element.
+        /// </summary>
+        /// <param name="element">The element to modify</param>
+        /// <param name="modifier">The number of dice that will be added. Negative to subtract, null to make immune.</param>
+        public void SetElementModifier(AttackElement element, int? modifier)
+        {
+            if (modifier == null && elementModifiers.ContainsKey(element))
+            {
+                elementModifiers.Remove(element);
+            } else if (modifier != null)
+            {
+                if (!elementModifiers.ContainsKey(element))
+                {
+                    elementModifiers.Add(element, (int)modifier);
+                } else
+                {
+                    elementModifiers[element] = (int)modifier;
+                }
+            }
+        }
+        /// <summary>
+        /// Checks if this unit is immune to an element.
+        /// </summary>
+        /// <param name="element">The element to check</param>
+        /// <returns>True if immune, false otherwise</returns>
+        public bool GetElementImmune(AttackElement element)
+        {
+            return !elementModifiers.ContainsKey(element);
+        }
+        /// <summary>
+        /// Sets whether or not this unit is immune to an element.
+        /// </summary>
+        /// <param name="element">The element to set</param>
+        /// <param name="immune">True to make immune, false to remove immunity</param>
+        public void SetElementImmune(AttackElement element, bool immune)
+        {
+            if (immune && elementModifiers.ContainsKey(element))
+            {
+                elementModifiers.Remove(element);
+            } else if (!immune && !elementModifiers.ContainsKey(element))
+            {
+                elementModifiers.Add(element, 0);
+            }
+        }
+        /// <summary>
+        /// The default constructor for a unit.
+        /// </summary>
+        public MWUnit()
+        {
+            foreach (AttackElement element in Enum.GetValues(typeof(AttackElement)))
+            {
+                elementModifiers.Add(element, 0);
             }
         }
     }
