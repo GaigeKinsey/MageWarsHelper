f29fa08e8b0f9f5a490a15af63be811abc1c3533
Gaige Kinsey <gaigekinsey@yahoo.com>
2019-11-15 14:56:23 -0700

    Added some stuff to test the database

---
+++ MageWarsHelper/Database/CardDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MageWarsHelper.Database
+{
+    public class CardDatabase
+    {
+        private CardDatabase() {}
+
+        private static CardDatabase instance = new CardDatabase();
+
+        public static CardDatabase Instance
+        {
+            get { return instance; }
+            set { instance = value; }
+        }
+
+        public void LoadDataBase()
+        {
+            List<MWCard> cards = new List<MWCard>();
+            string[] lines = System.IO.File.ReadAllLines("cardDatabase.txt");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
