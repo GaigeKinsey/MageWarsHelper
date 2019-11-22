4afdf1182c6515cdc428df363170f8b463f77149
EternityDoom <EternityDoom@users.noreply.github.com>
2019-11-22 15:16:31 -0700

    Updated the MWEquipment class. Equipment now has a Slot property, which stores the equipment location that it goes in.

--- MageWarsHelper/MW Elements/MWEquipment.cs
+++ MageWarsHelper/MW Elements/MWEquipment.cs
@@ -6,8 +6,61 @@ using System.Threading.Tasks;
 
 namespace MageWarsHelper
 {
+    public enum EquipmentLocation
+    {
+        HELMET,
+        CLOAK,
+        CHEST,
+        GLOVES,
+        BOOTS,
+        AMULET,
+        RING,
+        BELT,
+        SHIELD,
+        WEAPON,
+        ONE_HAND,
+        BOTH_HANDS
+    }
     public class MWEquipment : MWCard
     {
+        private EquipmentLocation slot = EquipmentLocation.RING;
+        /// <summary>
+        /// The slot that this Equipment goes in. Note that ONE_HAND
+        /// and BOTH_HANDS aren't true slots, but rather overlap with
+        /// either SHIELD, WEAPON, or both.
+        /// </summary>
+        public EquipmentLocation Slot { 
+            get {
+                return slot;
+            }
+            set
+            {
+                slot = value;
+                FieldChanged();
+            }
+        }
+        /// <summary>
+        /// The minumum range to cast this spell. All equipment has a min range of 0.
+        /// </summary>
+        public new int MinRange
+        {
+            get
+            {
+                return 0;
+            }
+            set { }
+        }
+        /// <summary>
+        /// The maximum range to cast this spell. All equipment has a max range of 2.
+        /// </summary>
+        public new int MaxRange
+        {
+            get
+            {
+                return 2;
+            }
+            set { }
+        }
         public override string CardType => "Equipment";
     }
 }
